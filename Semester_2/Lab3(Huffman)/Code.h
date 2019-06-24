c7434ead2268c67941950354843c117d31d46723
Nikssssss <43628920+Nikssssss@users.noreply.github.com>
2019-06-24 17:25:51 +0700

    Update Code.h

--- Semester_2/Lab3(Huffman)/Code.h
+++ Semester_2/Lab3(Huffman)/Code.h
@@ -2,21 +2,12 @@
 #define HUFFMAN_CODE_H
 #include "Structs.h"
 
-queue *newqueue(tree *node);
-tree *newtree(unsigned char sym, int freq);
-Code *newCode(unsigned char* sym, int size);
-void push(queue **sortedqueue, tree *node);
-tree *pop(queue **head);
+Code *newCode(unsigned char* sym, const int size);
 void countFreq(int* freqTable, FILE *in);
 void buildBase(queue **queue, int* freqTable);
-tree *merge(tree *first, tree *second);
 tree *buildCodetree(queue **queue);
-void writeSize(unsigned int size, FILE *out);
-void writeBits(unsigned char bits, int count, BitStream *bitStream, FILE *out);
+void writeSize(const unsigned int size, FILE *out);
 void writetree(tree *codetree, BitStream *bitStream, FILE *out);
-void resizeBuffer(Code *buffer);
-void addBit(Code *buffer, int bit);
-unsigned char* copyCode(Code *buffer);
 void buildCodeTable(Code **codeTable, tree *codetree, Code *buffer);
 void codeText(FILE *in, FILE *out, Code **codeTable, BitStream *bitStream);
 
