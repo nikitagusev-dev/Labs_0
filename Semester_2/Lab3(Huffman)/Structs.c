da5175601a5a591e47231f700cb8298cf450ad0d
Nikssssss <43628920+Nikssssss@users.noreply.github.com>
2019-06-24 17:28:51 +0700

    Update Structs.c

--- Semester_2/Lab3(Huffman)/Structs.c
+++ Semester_2/Lab3(Huffman)/Structs.c
@@ -1,6 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "Structs.h"
 
-BitStream *newBitStream() {
+queue *newqueue(tree *node) {
+	queue *tmp = (queue*)malloc(sizeof(queue));
+	tmp->next = NULL;
+	tmp->root = node;
+	return tmp;
+}
+
+tree *newtree(const unsigned char sym, const int freq) {
+	tree *tmp = (tree*)malloc(sizeof(tree));
+	tmp->freq = freq;
+	tmp->sym = sym;
+	tmp->left = NULL;
+	tmp->right = NULL;
+	return tmp;
+}
+
+void push(queue **sortedqueue, tree *node) {
+	int val = node->freq;
+	queue *tmp = newqueue(node);
+	if (!(*sortedqueue) || (*sortedqueue)->root->freq >= val) {
+		tmp->next = *sortedqueue;
+		*sortedqueue = tmp;
+		return;
+	}
+	queue *cur = *sortedqueue;
+	while (cur->next && val > cur->next->root->freq) {
+		cur = cur->next;
+	}
+	tmp->next = cur->next;
+	cur->next = tmp;
+}
+
+tree *pop(queue **head) {
+	queue *tmp = *head;
+	tree *sym = tmp->root;
+	*head = (*head)->next;
+	free(tmp);
+	return sym;
+}
+
+BitStream *newBitStream(void) {
 	BitStream *tmp = (BitStream*)malloc(sizeof(BitStream));
 	tmp->byte = 0;
 	tmp->size = 0;
