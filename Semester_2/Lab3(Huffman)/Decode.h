566b01816ce0de7f630122a157eeab552592d6e1
Nikssssss <43628920+Nikssssss@users.noreply.github.com>
2019-06-24 17:27:54 +0700

    Update Decode.h

--- Semester_2/Lab3(Huffman)/Decode.h
+++ Semester_2/Lab3(Huffman)/Decode.h
@@ -5,6 +5,6 @@
 int readSize(FILE *in, unsigned int *size);
 unsigned char getBit(BitStream *bitStream, FILE *in);
 tree *getCodetree(FILE *in, BitStream *bitStream);
-void decodeText(tree *codetree, FILE *in, FILE *out, unsigned int size, BitStream *bitStream);
+void decodeText(tree *codetree, FILE *in, FILE *out, const unsigned int size, BitStream *bitStream);
 
 #endif
