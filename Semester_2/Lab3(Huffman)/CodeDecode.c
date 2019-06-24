db69090f3e496fc456f1866478eef7f93239a92d
Nikssssss <43628920+Nikssssss@users.noreply.github.com>
2019-06-24 17:26:26 +0700

    Update CodeDecode.c

--- Semester_2/Lab3(Huffman)/CodeDecode.c
+++ Semester_2/Lab3(Huffman)/CodeDecode.c
@@ -1,4 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
 #include "CodeDecode.h"
+#define MAX_SIZE 256
+
+static void freeCodeTable(Code **codeTable) {
+	for (int i = 0; i < MAX_SIZE; i++) {
+		if (codeTable[i]) {
+			free(codeTable[i]->sym);
+			free(codeTable[i]);
+		}
+	}
+}
+
+static void freetree(tree *tree) {
+	if (!tree) {
+		return;
+	}
+	if (tree->left) {
+		freetree(tree->left);
+	}
+	if (tree->right) {
+		freetree(tree->right);
+	}
+	free(tree);
+}
 
 void code(FILE *in, FILE *out) {
 	int freqTable[MAX_SIZE];
@@ -7,7 +33,7 @@ void code(FILE *in, FILE *out) {
 	tree *codetree = NULL;
 	BitStream *bitStream = newBitStream();
 	memset(freqTable, 0, sizeof(int) * MAX_SIZE);
-	memset(codeTable, NULL, sizeof(Code*) * MAX_SIZE);
+	memset(codeTable, 0, sizeof(Code*) * MAX_SIZE);
 	countFreq(freqTable, in);
 	buildBase(&queue, freqTable);
 	codetree = buildCodetree(&queue);
@@ -33,26 +59,4 @@ void decode(FILE *in, FILE *out) {
 	decodeText(codetree, in, out, size, bitStream);
 	free(bitStream);
 	freetree(codetree);
-}
-
-void freeCodeTable(Code **codeTable) {
-	for (int i = 0; i < MAX_SIZE; i++) {
-		if (codeTable[i]) {
-			free(codeTable[i]->sym);
-			free(codeTable[i]);
-		}
-	}
-}
-
-void freetree(tree *tree) {
-	if (!tree) {
-		return;
-	}
-	if (tree->left) {
-		freetree(tree->left);
-	}
-	if (tree->right) {
-		freetree(tree->right);
-	}
-	free(tree);
 }
